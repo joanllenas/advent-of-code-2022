733e1a3f3b79feefbeb272cc67899540717b7db3
Joan Llenas Masó <joan.llenas.maso@gmail.com>
2022-12-02 16:36:48 +0100

    Day 1 part 1

--- ts/src/d1/main.ts
+++ ts/src/d1/main.ts
@@ -1,42 +1,22 @@
 import { readFileSync } from 'fs';
 
-const add = (n1: number, n2: number) => n1 + n2;
-
-function d1p1() {
-  const input = readFileSync('src/d1/input.txt', { encoding: 'utf-8' });
-  const lines = input.split('\n');
-  const groups = lines.reduce(
-    (acc, cur) => {
+function d1p1(input: string) {
+  return input
+    .split('\n')
+    .reduce((acc, cur) => {
       return cur === ''
-        ? [...acc, []]
-        : [
+        ? [...acc, 0]
+        : acc.length > 0
+        ? [
             ...acc.slice(0, acc.length - 1),
-            [...acc[acc.length - 1], parseInt(cur, 10)],
-          ];
-    },
-    [[]] as number[][],
-  );
-
-  const getIndex = (arr: number[]) => arr[0];
-  const getCalories = (arr: number[]) => arr[1];
-
-  const result = groups.reduce(
-    (prev, cur, index) => {
-      const curCalories = cur.reduce(add);
-      return getIndex(prev) > -1
-        ? curCalories > getCalories(prev)
-          ? [index, curCalories]
-          : prev
-        : [index, cur.reduce(add)];
-    },
-    [-1, 0],
-  );
-
-  console.log(
-    `Elf index ${getIndex(result)} has the most calories with ${getCalories(
-      result,
-    )} cal`,
-  );
+            acc[acc.length - 1] + parseInt(cur, 10),
+          ]
+        : [parseInt(cur, 10)];
+    }, [] as number[])
+    .sort((a, b) => a - b)
+    .reverse()[0];
 }
 
-d1p1();
+// Day 1, part 1
+// 67658
+console.log(d1p1(readFileSync('src/d1/input.txt', { encoding: 'utf-8' })));
