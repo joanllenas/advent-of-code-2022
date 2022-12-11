ba25d87e77837296a071449ddaf92b126087e096
Joan Llenas Masó <joan.llenas.maso@gmail.com>
2022-12-11 10:51:27 +0100

    Day 8

--- ts/src/d8/main.ts
+++ ts/src/d8/main.ts
@@ -1,12 +1,18 @@
 import { readFileSync } from 'fs';
 
-interface Tree {
+// --------------------
+//
+// part 1
+//
+// --------------------
+
+interface TreeVisibility {
   height: number;
   visible: boolean;
 }
 
 const isVisible = (
-  arr: Tree[][],
+  arr: TreeVisibility[][],
   colIndex: number,
   rowIndex: number,
 ): boolean => {
@@ -49,7 +55,6 @@ const isVisible = (
   return [left, right, top, bottom].includes(true);
 };
 
-// part 1
 function p1(input: string) {
   return input
     .split('\n')
@@ -57,7 +62,8 @@ function p1(input: string) {
       line
         .split('')
         .map(
-          (height) => ({ height: parseInt(height), visible: false } as Tree),
+          (height) =>
+            ({ height: parseInt(height), visible: false } as TreeVisibility),
         ),
     )
     .reduce((acc, cur, rowIndex, arr) => {
@@ -68,25 +74,107 @@ function p1(input: string) {
           visible: isVisible(arr, colIndex, rowIndex),
         })),
       ];
-    }, [] as Tree[][])
+    }, [] as TreeVisibility[][])
     .flat()
     .filter((tree) => tree.visible).length;
 }
 
+// --------------------
+//
 // part 2
-function p2(input: string) {
-  return '--';
+//
+// --------------------
+
+interface TreeScore {
+  height: number;
+  score: number;
 }
 
+const treeScore = (
+  arr: TreeScore[][],
+  colIndex: number,
+  rowIndex: number,
+): number => {
+  if (
+    colIndex === 0 ||
+    rowIndex === 0 ||
+    colIndex === arr[0].length - 1 ||
+    rowIndex === arr.length - 1
+  ) {
+    return 0;
+  }
+
+  const { height } = arr[rowIndex][colIndex];
+
+  const left = arr[rowIndex].reduce((acc, cur, index) => {
+    if (index < colIndex) {
+      acc = cur.height >= height ? 1 : acc + 1;
+    }
+    return acc;
+  }, 0);
+
+  const right = arr[rowIndex].reduceRight((acc, cur, index) => {
+    if (index > colIndex) {
+      acc = cur.height >= height ? 1 : acc + 1;
+    }
+    return acc;
+  }, 0);
+
+  const top = arr.reduce((acc, cur, index) => {
+    if (index < rowIndex) {
+      acc = cur[colIndex].height >= height ? 1 : acc + 1;
+    }
+    return acc;
+  }, 0);
+
+  const bottom = arr.reduceRight((acc, cur, index) => {
+    if (index > rowIndex) {
+      acc = cur[colIndex].height >= height ? 1 : acc + 1;
+    }
+    return acc;
+  }, 0);
+
+  return [left, right, top, bottom].reduce((acc, cur) => acc * cur);
+};
+
+function p2(input: string) {
+  return input
+    .split('\n')
+    .map((line) =>
+      line
+        .split('')
+        .map((height) => ({ height: parseInt(height), score: 0 } as TreeScore)),
+    )
+    .reduce((acc, cur, rowIndex, arr) => {
+      return [
+        ...acc,
+        cur.map((tree, colIndex) => ({
+          ...tree,
+          score: treeScore(arr, colIndex, rowIndex),
+        })),
+      ];
+    }, [] as TreeScore[][])
+    .flat()
+    .map((tree) => tree.score)
+    .sort((a, b) => a - b)
+    .reverse()
+    .at(0);
+}
+
+// --------------------
+//
 // run
+//
+// --------------------
+
 const input: string = readFileSync(__dirname + '/input.txt', {
   encoding: 'utf-8',
 });
 
 // part 1
 // 1681
-console.log(p1(input));
+console.log('p1', p1(input));
 
 // part 2
-// ??
-console.log(p2(input));
+// 201684
+console.log('p2', p2(input));
